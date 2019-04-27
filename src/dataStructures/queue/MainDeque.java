22d32188e40340284ab54c331596b8b9b7f588ae
Dmitry <rdi81@yandex.ru>
2019-04-27 23:13:53 +0300

    list

--- src/dataStructures/queue/MainDeque.java
+++ src/dataStructures/queue/MainDeque.java
@@ -3,19 +3,19 @@ package dataStructures.queue;
 public class MainDeque {
     public static void main(String[] args) {
         Deque dq = new Deque(3);
-        dq.pushFront(1);
-        dq.pushFront(2);
-        dq.pushFront(3);
-
-
-        while (!dq.isEmpty()) {
-            int n = dq.popFront();
-            System.out.println(n);
-        }
+//        dq.pushFront(1);
+//        dq.pushFront(2);
+//        dq.pushFront(3);
+//
+//
+//        while (!dq.isEmpty()) {
+//            int n = dq.popFront();
+//            System.out.println(n);
+//        }
 
         dq.pushBack(4);
-        dq.pushBack(5);
-        dq.pushBack(6);
+//        dq.pushBack(5);
+//        dq.pushBack(6);
 
         while (!dq.isEmpty()) {
             int n = dq.popBack();
