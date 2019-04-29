9a18aa004807a434cebfa1c2194dabf384a59b4c
Dmitry <rdi81@yandex.ru>
2019-04-29 22:44:35 +0300

    three

---
+++ src/dataStructures/three/Node.java
@@ -0,0 +1,11 @@
+package dataStructures.three;
+
+public class Node {
+    public Person person;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void display() {
+        System.out.println("Name " + person.name + " ,age " + person.age);
+    }
+}
